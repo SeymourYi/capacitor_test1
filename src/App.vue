1a4b55b5a87f7648e8c4a90971c32957d798357f
John Doe <john@example.com>
2025-10-24 13:13:49 +0800

    web

--- src/App.vue
+++ src/App.vue
@@ -36,6 +36,7 @@ onMounted(() => {
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
+    web
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
